00e252d5478b0c826317b8317b40b74e292356cf
YF <weiyufei2014@outlook.com>
2022-06-24 14:55:42 +0200

    Add files via upload

---
+++ Tab_MaxUHI_LOGPOP_OLS_withInter.r
@@ -0,0 +1,19 @@
+options(digits=10)
+library(xlsx)
+library(lmtest)
+library(openxlsx)
+data1 <- read.xlsx("M://maintextTable.xlsx", sheet=1)
+
+maxUHI<-data1$MaxUHI
+popSize<-data1$Population
+
+logPopSize<-log(popSize,base=10)
+
+model1<-lm(maxUHI~logPopSize)	
+
+summary(model1)
+manualR<-1-deviance(model1)/sum(anova(model1)[,2])
+manualR
+
+
+bptest(model1)
