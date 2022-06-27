2a5c0d6745759c49a9f4b96e9beb13da54964172
YF <weiyufei2014@outlook.com>
2022-06-27 21:26:13 +0200

    Add files via upload

---
+++ Tab_statistics_of _data_part1.r
@@ -0,0 +1,43 @@
+options(digits = 10)
+library(openxlsx)
+
+data1 <- read.xlsx("M://Regression_Station.xlsx", sheet=1)
+
+popSizeStat<-data1$FUA_p_2015
+areaStat<-data1$FUA_area
+NO2Station<-data1$AQValue
+distCenterSation<-data1$DisStaCen
+
+std <- function(x) sd(x)/sqrt(length(x))
+
+print ("Column-Population")
+
+round(min(unique(popSizeStat)),1)
+round(max(unique(popSizeStat)),1)
+round(mean(unique(popSizeStat)),1)
+round(sd(unique(popSizeStat)),1)
+round(std(unique(popSizeStat)),1)
+
+print ("Column-Area")
+
+round(min(unique(areaStat)),1)
+round(max(unique(areaStat)),1)
+round(mean(unique(areaStat)),1)
+round(sd(unique(areaStat)),1)
+round(std(unique(areaStat)),1)
+
+print("Column-Annual mean NO2 station")
+
+round(min(NO2Station),1)
+round(max(NO2Station),1)
+round(mean(NO2Station),1)
+round(sd(NO2Station),1)
+round(std(NO2Station),1)
+
+print("Column-Dist station")
+
+round(min(distCenterSation),1)
+round(max(distCenterSation),1)
+round(mean(distCenterSation),1)
+round(sd(distCenterSation),1)
+round(std(distCenterSation),1)
