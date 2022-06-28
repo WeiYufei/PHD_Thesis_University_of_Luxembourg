b5e1702c46faa3bc9abfe03514508af64c268ecc
YF <weiyufei2014@outlook.com>
2022-06-28 09:43:12 +0200

    Add files via upload

---
+++ Tab_Satellite_Station_Pop_C2.r
@@ -0,0 +1,21 @@
+options(digits = 10)
+library(lmtest)
+library(openxlsx)
+library(sandwich)
+library(relaimpo)
+
+data1 <- read.xlsx("M:/Eclipse_Workspace/Article1_NC_R/Regression_OverlappedCell_w_Station.xlsx", sheet=1)
+popSize<-data1$Pop2015
+meanNO2cell<-data1$grid_code
+meanNO2station<-data1$AQValue
+
+
+model1<-lm(meanNO2cell~meanNO2station+popSize)
+summary(model1)
+
+manualR<-1-deviance(model1)/sum(anova(model1)[,2])
+manualR
+
+coeftest(model1, vcov = vcovHC(model1, type = "HC1"))
+
+calc.relimp(model1,rela=TRUE)
