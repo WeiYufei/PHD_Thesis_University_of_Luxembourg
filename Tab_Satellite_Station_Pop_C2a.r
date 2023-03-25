40693db15a8f433dca1815d409ac0a241deead17
WEI Yufei <weiyufei2014@outlook.com>
2023-03-25 12:29:50 +0100

    Add files via upload

---
+++ Tab_Satellite_Station_Pop_C2a.r
@@ -0,0 +1,36 @@
+options(digits = 10)
+library(lmtest)
+library(margins)
+library(openxlsx)
+library(relaimpo)
+library(sandwich)
+
+data1 <- read.xlsx("M:/Eclipse_Workspace/Article1_Py_NRS/overlappedCellImproved.xlsx", sheet=1)
+
+popSizeDr<-data1$FUA_p_2015
+popSize151<-round(popSizeDr)
+popSize15<-log(popSize151,base=10)
+
+meanNO2cell1<-data1$grid_code
+meanNO2cell<-log(meanNO2cell1,base=10)
+
+meanNO2station1<-data1$AQValue
+meanNO2station<-log(meanNO2station1,base=10)
+
+
+
+###model
+###################################################################
+model1<-lm(meanNO2cell~meanNO2station+popSize15)
+summary(model1)
+anova(model1)
+calc.relimp(model1,rela=TRUE)
+bootresults<-boot.relimp(model1,b=1000)
+ci<-booteval.relimp(bootresults,norank=T)
+ci
+####
+manualR<-1-deviance(model1)/sum(anova(model1)[,2])
+manualR
+
+coeftest(model1, vcov = vcovHC(model1, type = "HC0"))
+############################################################################
